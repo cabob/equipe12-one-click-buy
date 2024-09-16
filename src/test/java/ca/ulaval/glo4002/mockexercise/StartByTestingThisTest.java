9e3588af58867d824a9d26246b123d8556ac073c
Carl Boulianne-Boivin <lordboivin@gmail.com>
2024-09-16 19:09:07 -0400

    first commit

--- src/test/java/ca/ulaval/glo4002/mockexercise/StartByTestingThisTest.java
+++ src/test/java/ca/ulaval/glo4002/mockexercise/StartByTestingThisTest.java
@@ -1,13 +1,27 @@
 package ca.ulaval.glo4002.mockexercise;
 
+import ca.ulaval.glo4002.mockexercise.do_not_edit.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class StartByTestingThisTest {
 
+    private static final Product produit = new Product("sku", "name", 0);
     private StartByTestingThis service;
+    private ProductRepository repository;
 
     @BeforeEach
     public void setUp() {
         service = new StartByTestingThis();
+        repository = mock(ProductRepository.class);
+        when(repository.findBySku("sku")).thenReturn(produit);
+    }
+
+    @Test
+    public void ajoutNouveauProduitCreePanierEtRetourneFacture() {
+        Product product = new Product("sku", "name", 0);
     }
 }
